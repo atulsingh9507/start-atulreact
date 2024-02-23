893f833caf824cb6c93e3b2643fd04c82d9431b2
atul kumar singh <atulsingh9507@gmail.com>
2024-02-23 14:53:56 +0530

    add click button

--- src/component/person.js
+++ src/component/person.js
@@ -5,12 +5,24 @@ class person extends Component {
   constructor(props) {
     super(props);
     this.props=props;
-  }
+    this.state={
+        name:"atul kumar singh",
+        age: 22,
+    }
+}
+nameChangeHandeler() {
+    console.log(this.state);
+    this.setState({ name: "vikash singh", age:27 }, () => {
+        console.log(this.state);
+    });
+}
    render() {
     return(
         <div>
             <h1>
-                <i>Dear {this.props.name},{this.props.age} year, welcome atul</i>
+                <i>Dear {this.state.name},{this.state.age} year, welcome atul</i>
+                <br/>
+                <button onClick={() => this.nameChangeHandeler()}>click me </button>
             </h1>
         </div>
     );
